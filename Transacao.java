f15018c6ceb8c565c18ccb5d12f2b3869d6c861a
heitorfc <101341627+heitorfc@users.noreply.github.com>
2022-09-13 11:45:23 -0300

    Add files via upload

---
+++ Transacao.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+class Transacao {
+    // ATRIBUTOS
+    private String id;
+    private LocalDate data;
+    private String idRementente;
+    private String idDestinatario;
+}
