ce5d85c85dd7ea17ea279c1cc8a0e197ef28e250
Casalbe <83553693+Casalbe@users.noreply.github.com>
2022-09-15 18:49:33 -0300

    Delete Transacao.java

--- back-end/src/main/java/com/rubank/classes/Transacao.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-
-class Transacao {
-    // ATRIBUTOS
-    private String id;
-    private LocalDate data;
-    private String idRementente;
-    private String idDestinatario;
-}
