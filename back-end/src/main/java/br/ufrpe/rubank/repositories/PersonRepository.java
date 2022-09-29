40dec4e2f742d1c735c00f838cc9358809969225
Casalbe <83553693+Casalbe@users.noreply.github.com>
2022-09-29 16:34:02 -0300

    Delete back-end directory
    Apagando a versao antiga do back end para commitar a final

--- back-end/src/main/java/br/ufrpe/rubank/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package br.ufrpe.rubank.repositories;
-
-import br.ufrpe.rubank.models.Person;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonRepository extends MongoRepository<Person, String> {
-
-}
